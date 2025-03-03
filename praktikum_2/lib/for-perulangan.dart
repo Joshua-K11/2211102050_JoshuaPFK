92d46087284f916650654c70c2ce4018783c51fc
Joshua <joshua321@gmail.com>
2025-03-03 16:46:50 +0700

    prak2

---
+++ praktikum_2/lib/for-perulangan.dart
@@ -0,0 +1,9 @@
+void main() {
+  for (int i = 21102200; i <= 21102210; i++) {
+    print('Mahasiswa ke-$i');
+  }
+  var mahasiswa = ['Ananda', 'Surya', 'Kusuma', 'Wahyu', 'Pram'];
+  mahasiswa.forEach((nama) {
+    print('Nama Mahasiswa: $nama');
+  });
+}
