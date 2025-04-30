948b0a7a8eeffa44f4fb5dfbd32f0739b0d2ec2d
Joshua <joshua321@gmail.com>
2025-04-30 10:40:13 +0700

    Tugas-dasar

---
+++ praktikum_10/Tugas Dasar/proses.php
@@ -0,0 +1,8 @@
+<?php
+if ($_POST['age'] <= 18) {
+  echo "Kurang gede dek";
+  echo "<br>";
+} else {
+  echo "Dewasa";
+}
+?>
---
+++ praktikum_10/Tugas Dasar/webform.php
@@ -0,0 +1,7 @@
+<form method="POST" action="proses.php"> 
+  Umur : <input type="int" name="age" required> <br>  
+  <input type="submit" value="Input"> 
+</form>
+<?php
+
+?>
