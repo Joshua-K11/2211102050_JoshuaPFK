51fac4ed553f937d4c575188227d2239b3aaea09
Joshua <joshua321@gmail.com>
2025-04-30 09:02:03 +0700

    praktikum 10

---
+++ praktikum_10/GET POST/request.php
@@ -0,0 +1,15 @@
+<?php
+echo '<form method="GET" action="proses.php">';
+echo 'Nama: <input type="text" name="nama">';
+echo '<input type="submit">';
+echo '</form>';
+echo '<br>';
+?>
+
+<?php
+echo '<form method="POST" action="proses2.php">';
+echo 'Nama: <input type="text" name="nama">';
+echo '<input type="submit">';
+echo '</form>';
+echo '<br>';
+?>
