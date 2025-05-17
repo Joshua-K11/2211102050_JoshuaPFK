470e584c968adf8105652db2115c1083ea60e124
Joshua <joshua321@gmail.com>
2025-05-17 10:28:51 +0700

    praktikum 12

--- praktikum_12/car_laravel/routes/web.php
+++ praktikum_12/car_laravel/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('car', CarController::class);
